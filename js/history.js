b2416643fc7d915c6b5a0818311429d82a06a367
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-17 23:17:45 +0530

    Allow users to save their operations

---
+++ js/history.js
@@ -0,0 +1,22 @@
+var matrix_operations_history = [];
+
+function saveToHistory(matrix, description = "") {
+    history_addition = {
+        "description": description,
+        "matrix": matrix
+    };
+    matrix_operations_history.push(history_addition);
+}
+
+function undo() {
+    return undo_changes(matrix_operations_history, document.getElementById("matrix_placeholder"))
+}
+
+function undo_changes(m, p) {
+    var returned = m.pop();
+    p.innerHTML = "";
+    for (var i = 0; i < m.length; i++) {
+        render(m[i]["matrix"], p, 1, 0, m[i]["description"]);
+    }
+    return returned;
+}
